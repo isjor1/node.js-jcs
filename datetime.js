470e33c6bf0eec028e1b2151dcde79b50b62cae4
isjor1 <47063600+isjor1@users.noreply.github.com>
2020-11-13 15:48:52 -0600

    Add files via upload

--- datetime.js
+++ datetime.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 exports.myDateTime = function () {
     return Date();
-=======
-exports.myDateTime = function () {
-    return Date();
->>>>>>> d7b26c4557ecccfccbeece2cd80581e80c7fccf6
 }; 
